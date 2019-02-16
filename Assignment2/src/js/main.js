18f59e7057e22a123f64dc8d4714d9f0f7ffb083
Gabrielle Ashley <a.gabby1219@gmail.com>
2019-02-15 23:43:31 -0600

    final touches

--- Assignment2/src/js/main.js
+++ Assignment2/src/js/main.js
@@ -1,6 +1,4 @@
-//import React from "react";
-let root = document.getElementById("root");
-
+const root = document.getElementById("root");
 
 const App = () => {
     return React.createElement('div', { className: "text-center bg-primary text-white p-4 border rounded border-dark", style: { margin: 'auto', marginTop: 50, width: 300, height: 200 } },
