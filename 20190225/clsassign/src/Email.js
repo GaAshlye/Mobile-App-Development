b7aa904f934e5634edf6d337a9683924aa426947
Gabrielle Ashley <a.gabby1219@gmail.com>
2019-02-28 20:42:12 -0600

    initial commit, with email componenet

---
+++ 20190225/clsassign/src/Email.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+
+class Email extends Component {
+
+    constructor(props){
+        super(props);
+
+
+        this.state ={
+            email: ''
+        }
+        
+        this.onEmailChange = this.onEmailChange.bind(this);
+    }
+   
+    onEmailChange(event) {
+        const email = event.target.value;
+
+        //lift the state value to parent (form)
+        this.props.onEmailInputChange(email); //need to call onEmailInputChange in parent
+
+
+        this.setState( () =>{
+            return {
+                email
+            }
+        });
+
+
+    };
+    
+    render() { 
+        return ( 
+            <div className="form-group">
+             <label for="exampleInputEmail1">Email address</label>
+                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" value={this.state.email} onChange={this.onEmailChange}></input>
+                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
+          );
+    }
+}
+ 
+export default Email;
+
